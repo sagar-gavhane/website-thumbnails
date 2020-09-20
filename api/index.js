b8007498df1b356762292baff8a8f76f2628bab1
Sagar Gavhane <sgavhane70@gmail.com>
2020-09-20 18:52:58 +0530

    print errors

--- api/index.js
+++ api/index.js
@@ -6,32 +6,36 @@ const badRequestImage = fs.readFileSync(pathToImage)
 
 module.exports = async (req, res) => {
   // if url is undefined then send 500 bad request image
-  if(typeof req.query.url === 'undefined') {
-    res.setHeader('Content-Type', 'image/png');
+  if (typeof req.query.url === 'undefined') {
+    res.setHeader('Content-Type', 'image/png')
     res.send(badRequestImage)
-    return;
+    return
   }
 
   try {
     const url = new URL(req.query.url)
-    const chrome = require('chrome-aws-lambda');
-    const puppeteer = require('puppeteer');
+    const chrome = require('chrome-aws-lambda')
+    const puppeteer = require('puppeteer')
     const browser = await puppeteer.launch({
       args: chrome.args,
       executablePath: await chrome.executablePath,
       headless: chrome.headless,
-  });
-    const page = await browser.newPage();
-    await page.goto(url);
-    const capturedImage = await page.screenshot({type: 'jpeg', quality: 100, fullPage: false});
-    await browser.close();
+    })
+    const page = await browser.newPage()
+    await page.goto(url)
+    const capturedImage = await page.screenshot({
+      type: 'jpeg',
+      quality: 100,
+      fullPage: false,
+    })
+    await browser.close()
 
-    res.setHeader('Content-Type', 'image/jpeg');
+    res.setHeader('Content-Type', 'image/jpeg')
     res.send(capturedImage)
   } catch (err) {
-    console.log('[err]', err)
-    res.setHeader('Content-Type', 'image/png');
+    console.error('[err]', err)
+    res.setHeader('Content-Type', 'image/png')
     res.send(badRequestImage)
-    return;
+    return
   }
 }
